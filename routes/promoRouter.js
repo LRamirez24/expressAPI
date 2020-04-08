50e33ab9eb2e991878622879a2af608da8e2f706
luis Ramirez <l.r.32@hotmail.com>
2020-04-08 12:07:59 -0700

    assignment 2 coursera

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -1,49 +1,91 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Promotions = require('../models/promotions');
 
 const promoRouter = express.Router();
-
 promoRouter.use(bodyParser.json());
 
 promoRouter.route('/')
-    .all((req, res, next) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        next();
-    })
     .get((req, res, next) => {
-        res.end('Will send all the promos to you!');
+
+        Promotions.find({})
+            .then((promos) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(promos);
+                },
+                (err) => next(err))
+            .catch((err) => next(err));
     })
     .post((req, res, next) => {
-        res.end('Will add the promo: ' + req.body.name + ' with details: ' + req.body.description);
+
+        Promotions.create(req.body)
+            .then((promo) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(promo);
+                },
+                (err) => next(err))
+            .catch((err) => next(err));
     })
     .put((req, res, next) => {
+
         res.statusCode = 403;
         res.end('PUT operation not supported on /promotions');
     })
     .delete((req, res, next) => {
-        res.end('Deleting all promotions');
+
+        Promotions.remove({})
+            .then((response) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(response);
+                },
+                (err) => next(err))
+            .catch((err) => next(err));
     });
 
-promoRouter.route('/:promoOld')
+promoRouter.route('/:promoId')
+    .get((req, res, next) => {
 
-.get((req, res, next) => {
-    res.end('Will send details of the promotion: ' + req.params.promoOld + ' to you!');
-})
+        Promotions.findById(req.params.promoId)
+            .then((promo) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(promo);
+                },
+                (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .post((req, res, next) => {
+        res.statusCode = 403;
+        res.end('POST operation not supported on /promotions/' + req.params.promoId);
+    })
+    .put((req, res, next) => {
 
-.post((req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /promotionss/' + req.params.promoOld);
-})
+        Promotions.findByIdAndUpdate(req.params.promoId, {
+                $set: req.body
+            }, { new: true })
+            .then((promo) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(promo);
+                },
+                (err) => next(err))
+            .catch((err) => next(err));
+    })
+    .delete((req, res, next) => {
 
-.put((req, res, next) => {
-    res.write('Updating the promotion: ' + req.params.promoOld + '\n');
-    res.end('Will update the promotion: ' + req.body.name +
-        ' with details: ' + req.body.description);
-})
-
-.delete((req, res, next) => {
-    res.end('Deleting promotion: ' + req.params.promoOld);
-});
+        Promotions.findByIdAndRemove(req.params.promoId)
+            .then((response) => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(response);
+                },
+                (err) => next(err))
+            .catch((err) => next(err));
+    });
 
 module.exports = promoRouter;
